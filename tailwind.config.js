b61374818c4d08633c83d1a040632fc179898c11
Arnav <arnavsharma1104@gmail.com>
2025-07-15 00:22:28 +0530

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Adjust if you use JSX/TSX/etc.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
